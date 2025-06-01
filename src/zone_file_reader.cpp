4e9dbaa3f4a6cdcdf6ed9be81cdd7dc8831ce3ed
Tahsinzisan <71321070+tahsinzisan@users.noreply.github.com>
2025-06-01 06:35:00 +0600

    Fix file reader

--- src/zone_file_reader.cpp
+++ src/zone_file_reader.cpp
@@ -1,11 +1,18 @@
-#include "../include/zone_file_reader.h"
+#include "zone_file_reader.h"
 #include <fstream>
 #include <sstream>
 #include <iostream>
 using namespace std;
 
-ZoneFileReader::ZoneFileReader(const std::string& zone_file_path) 
-    : zone_file_path_(zone_file_path) {}
+
+ZoneFileReader::ZoneFileReader(const std::string& filename) {
+    zone_file_path_ = filename;
+    
+    // Debug output - shows EXACT path being used
+    std::cout << "DEBUG: Trying to open zone file at: " 
+              << std::filesystem::absolute(zone_file_path_) << std::endl;
+}
+
 
 bool ZoneFileReader::load_zone_file() {
     ifstream file(zone_file_path_);
@@ -17,23 +24,28 @@ bool ZoneFileReader::load_zone_file() {
     string line;
     while (getline(file, line)) {
         istringstream iss(line);
-        string domain, record_type, internal_ip, external_ip;
-        if (iss >> domain >> record_type >> internal_ip >> external_ip) {
-            if (record_type == "IN" || record_type == "A") {
-                // Store both internal and external IPs for each domain
+        string domain, class_, record_type, internal_ip, external_ip;
+        if (iss >> domain >> class_ >> record_type >> internal_ip >> external_ip) {
+            if (!domain.empty() && domain.back() == '.') {
+                domain.pop_back();  
+            }
+            if (class_ == "IN" && record_type == "A") {
                 zone_records_[domain] = make_pair(internal_ip, external_ip);
             }
         }
-    }
-
+    }   
+    cerr << "success to open zone file: " <<  endl;
+    cerr<<zone_records_.size()<<endl;
     file.close();
     return true;
 }
 
+
 string ZoneFileReader::get_ip_for_domain(string& domain, bool is_internal) {
+    std::cerr << "returning ip for host" << std::endl;
     auto it = zone_records_.find(domain);
     if (it != zone_records_.end()) {
-        // Return the internal or external IP based on the `is_internal` flag
+        // will return the internal or external IP based on the `is_internal` flag
         return is_internal ? it->second.first : it->second.second;
     }
     return "";  // Return empty string if the domain is not found
