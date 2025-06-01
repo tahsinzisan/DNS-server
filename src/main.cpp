d5f0aede4a943b0c0c722c6967f03af7f7fd8543
Tahsinzisan <71321070+tahsinzisan@users.noreply.github.com>
2025-06-01 06:29:16 +0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,19 @@ using namespace boost::asio;
 using namespace std;
 
 int main() {
-
-    ZoneFileReader zone_reader("../zone.txt");
+    
+    ZoneFileReader zone_reader("zone.txt");
     if (!zone_reader.load_zone_file()) {
         std::cerr << "Failed to load zone file!" << std::endl;
         return -1; 
     }
+
     try {
-        io_service io_service;
-        DNS_Server server(io_service, 133, 133);  
-        server.run();  // Start the server 
-    } catch (const exception& e) {
-        cerr << "Error: " << e.what() << std::endl;
+        boost::asio::io_context io_service;
+        DNS_Server server(io_service, 133, 133, zone_reader);  // pass zone_reader here
+        server.run();
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
     }
 
     return 0;
