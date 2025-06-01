5c0531d712b58bb8ced99ff6e5fabb3cc6b04ee3
Tahsinzisan <71321070+tahsinzisan@users.noreply.github.com>
2025-06-01 07:28:43 +0600

    fix zone_file_reader.h

--- include/zone_file_reader.h
+++ include/zone_file_reader.h
@@ -9,11 +9,12 @@ class ZoneFileReader {
 public:
     ZoneFileReader(const string& zone_file_path);
     bool load_zone_file();
-    string get_ip_for_domain( string& domain);
+    string get_ip_for_domain(string& domain, bool is_internal);
 
 private:
     string zone_file_path_;
-    map<string, string> zone_records_;
+    map<string, pair<string, string>> zone_records_;
+
 };
 
 #endif 
